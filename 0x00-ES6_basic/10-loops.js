2bc470116064582c7aaf8af50cff4c976ccd13a7
SirJupiter <jupitertoby@gmail.com>
2024-07-22 21:51:29 +0100

    Tasks 1 - 12

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (let value of array) {
+    // Find the index using the findIndex method
+    const index = array.indexOf(value);
+    array[index] = appendString + value;
+  }
+
+  return array;
+}
