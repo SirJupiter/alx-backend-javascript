d1497083cfb2d7cb6bf0785976c9d1b69d611771
SirJupiter <jupitertoby@gmail.com>
2024-07-24 21:23:15 +0100

    8. Throw error / try catch

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+function divideFunction(numerator, denominator) {
+  if (denominator === 0) throw Error('cannot divide by 0');
+  return numerator / denominator;
+}
+
+export default divideFunction;
