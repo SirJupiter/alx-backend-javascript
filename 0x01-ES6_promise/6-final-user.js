71a38258507806a8e900b05375d26cd1ef7aaa61
SirJupiter <jupitertoby@gmail.com>
2024-07-24 13:16:43 +0100

    6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => console.log(data));
+}
+
+export default handleProfileSignup;
