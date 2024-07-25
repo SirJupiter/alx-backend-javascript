c894b6f34709a0fb117a0cf5ef42ae7e524865f0
SirJupiter <jupitertoby@gmail.com>
2024-07-25 22:30:04 +0100

    1. Let's make some classrooms

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+import Classroom from "./0-classroom";
+
+function intializeRooms() {
+  return [new Classroom(19), new Classroom(20), new Classroom(34)];
+}
+
+export default intializeRooms;
