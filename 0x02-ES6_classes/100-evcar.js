8a83c2a260518a19afc8d7788a602087fd476218
SirJupiter <jupitertoby@gmail.com>
2024-07-26 22:15:16 +0100

    11. EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
+
+export default EVCar;
