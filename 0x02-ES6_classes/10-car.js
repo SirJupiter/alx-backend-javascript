6aedcbd450496f0a492856c5f13a476cfd0f84ee
SirJupiter <jupitertoby@gmail.com>
2024-07-26 22:04:35 +0100

    10. Vroom

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -9,16 +9,38 @@ class Car {
     this._color = color;
   }
 
-  [cloneSymbol]() {
-    const clone = new (Object.getPrototypeOf(this).constructor)();
-    clone._brand = this._brand;
-    clone._motor = this._motor;
-    clone._color = this._color;
-    return clone;
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
   }
 
   cloneCar() {
-    return this[cloneSymbol]();
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
   }
 }
 
