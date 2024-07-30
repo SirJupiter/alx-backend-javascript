ab1ee7e073760378448b33a133e9415ad2c23153
SirJupiter <jupitertoby@gmail.com>
2024-07-30 10:34:05 +0100

    5. Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  if (position >= length) {
+    throw Error('Position outside range');
+  }
+  int8View[position] = value;
+  return new DataView(buffer);
+}
+
+export default createInt8TypedArray;
