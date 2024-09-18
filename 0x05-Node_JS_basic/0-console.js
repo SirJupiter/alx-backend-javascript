9a16327b301e3aef90c6ee74cb1b0a80200c54dc
SirJupiter <jupitertoby@gmail.com>
2024-09-18 20:48:18 +0100

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT
+ * @param {String} message: the message to display
+ */
+
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
