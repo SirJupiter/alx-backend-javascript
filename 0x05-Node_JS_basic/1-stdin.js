eac1dc2ec83cabdfce729f5fe8de112509decb50
SirJupiter <jupitertoby@gmail.com>
2024-09-21 11:13:26 +0100

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,14 +4,12 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const chk = process.stdin.read();
-
-  if (chk) {
-    process.stdout.write(`Your name is ${chk}`);
+process.stdin.on('data', (chunk) => {
+  if (chunk) {
+    process.stdout.write(`Your name is ${chunk.toString()}`);
   }
 });
 
-process.stdin.on('end', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
